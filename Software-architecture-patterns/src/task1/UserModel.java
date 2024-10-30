5541de3f06dd9dec11c47c492b9829305ba03fbf
Filanteyee <157284265+Filanteyee@users.noreply.github.com>
2024-10-30 16:12:12 +0500

    Add files via upload

---
+++ Software-architecture-patterns/src/task1/UserModel.java
@@ -0,0 +1,20 @@
+package task1;
+
+public class UserModel {
+    private String username;
+    private String password;
+
+    public UserModel(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
+
